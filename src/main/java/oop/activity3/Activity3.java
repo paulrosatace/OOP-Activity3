f55b42f037b6efef480c82b843a563870d5f9f1d
paulrosatace <paulrosatace@gmail.com>
2024-03-16 23:40:58 +0800

    activity3 commit

---
+++ src/main/java/oop/activity3/Activity3.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package oop.activity3;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Activity3 {
+
+    public static void main(String[] args) {
+         
+        double celsius = 40.0;
+        double fahrenheit = (celsius * 9 / 5) + 32;
+
+        System.out.println(celsius + " degrees Celsius is equal to " 
+                + fahrenheit +" degrees Fahrenheit.");
+    }
+}
